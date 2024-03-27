c38a90c4ca1a4a9328e0107f6e4e912a4e60bdd8
TamNhien <canhthien91@gmail.com>
2024-03-27 17:28:53 +0700

    Add project files.

---
+++ P_XoaChuoiBTrongA_NoiLai.py
@@ -0,0 +1,12 @@
+﻿def remove_substring(a, b):
+    while b in a:
+        a = a.replace(b, "", 1)
+    return a
+
+a = input("Nhập chuỗi a : ")
+b = input("Nhập chuỗi b : ")
+
+a = remove_substring(a, b)
+
+print("Chuỗi a sau khi xóa chuỗi b : ", a)
+
